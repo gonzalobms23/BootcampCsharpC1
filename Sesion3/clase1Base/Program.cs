c38cf030597db4fd69d64b5b1805a0931f8a7a5f
Gonzalo Mayhuire <gonzalobms23@gmail.com>
2024-09-04 20:29:52 -0500

    Actualizacion

--- Sesion3/clase1Base/Program.cs
+++ Sesion3/clase1Base/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hola! Alumnos");
+//Variables
+int edad = 20;
+long altura = 165;
+double dinero = 1651.36;
+float danio = 124.4f;
+decimal montoEnBanco = 1245151.4m;
+string nombre = "pedro";
+char letra = 'A';
+bool tieneGorro = false;
+var kilometros = 351531.5454;
+Console.WriteLine("Escribe tu nombre");
+nombre=Console.ReadLine();
+Console.WriteLine("Mi nombre es "+nombre +" mi edad es "+edad);
+Console.WriteLine($"Mi altura es {altura}");
+Console.WriteLine("Escribe tu Dinero");
+dinero =double.Parse(Console.ReadLine());
+Console.WriteLine($"tu dinero es {dinero}");
