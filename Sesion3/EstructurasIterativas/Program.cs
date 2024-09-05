f7cdfccb6f321002c6cf848eb3ab1bd31ab7ec00
Gonzalo Mayhuire <gonzalobms23@gmail.com>
2024-09-04 22:25:26 -0500

    estructura while finalizada

--- Sesion3/EstructurasIterativas/Program.cs
+++ Sesion3/EstructurasIterativas/Program.cs
@@ -1,10 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ingresa los numeros a sumar");
-int numero1 =int.Parse( Console.ReadLine());
-int numero2 =int.Parse( Console.ReadLine());
-int resultado = numero1 + numero2;
-Console.WriteLine($"Resultado es {resultado}" );
 
+
+using System;
+/*
 Console.WriteLine("Ingresa el numero limite WHILE");
 int numeroLimite = int.Parse(Console.ReadLine());
 int contador = 0;
@@ -22,11 +20,15 @@ do
     Console.WriteLine(contador);
 } while (contador<numeroLimite);
 
-
+*/
 int opcion = 0;
 
 while (opcion != 7)
 {
+    Console.WriteLine("Ingresa los numeros a Operar");
+    double numero1 = double.Parse(Console.ReadLine());
+    double numero2 = double.Parse(Console.ReadLine());
+    double resultado = 0;
     Console.WriteLine("Calculadora------->");
     Console.WriteLine("1.-Sumar");
     Console.WriteLine("2.-Restar");
@@ -37,12 +39,30 @@ while (opcion != 7)
     Console.WriteLine("7.-Salir");
     Console.WriteLine("Ingresa la opcion");
     opcion = int.Parse(Console.ReadLine());
-    if (opcion == 1)
+
+    switch (opcion)
     {
-        Console.WriteLine("Sumando");        
+        case 1:
+            resultado = numero1 + numero2;
+            break;
+        case 2:
+            resultado = numero1 - numero2;
+            break;
+        case 3:
+            resultado = numero1 * numero2;
+            break;
+        case 4:
+            if (numero2 == 0)
+            {
+                Console.WriteLine("No se puede dividir entre 0");
+                return;
+            }
+            resultado = numero1 / numero2;
+
+            break;
+        default:
+            break;
     }
-    if (opcion == 2)
-        Console.WriteLine("Resta");
-    
+    Console.WriteLine($"Resultado es {Math.Round(resultado,2)}");
 }
 Console.WriteLine("Salida");
